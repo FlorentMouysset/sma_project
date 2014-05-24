ea9b5e022c2da0cb13bee080d8d13cfd0880765c
Florent Mouysset <mouysset.florent@gmail.com>
2014-05-24 14:31:46 +0200

    #4 deb doc

--- src/rsma/interfaces/IEnvironnementActions.java
+++ src/rsma/interfaces/IEnvironnementActions.java
@@ -5,6 +5,9 @@ import rsma.util.Position;
 
 public interface IEnvironnementActions {
 
+	/**Move a robot from its old position to the new position </br>
+	 * The robot must check if the newPosition is free.
+	 * */
 	void moveRobot(Position oldPosition , Position newPosition);
 	void pullResource(Position position);
 	void pushResource(Position position);
