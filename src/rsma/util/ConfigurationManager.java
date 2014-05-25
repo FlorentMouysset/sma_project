667f60a4dbdbd6b898055886710ba6d2fa6a7569
Florent Mouysset <mouysset.florent@gmail.com>
2014-05-25 12:57:56 +0200

    #10 #1 ok ConfigurationManager + warehouse.properties + gui.properties deb

---
+++ src/rsma/util/ConfigurationManager.java
@@ -0,0 +1,48 @@
+package rsma.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class ConfigurationManager {
+	private static ConfigurationManager instance = null;
+	private Properties prop;
+	
+	private ConfigurationManager(){
+		
+	}
+	
+	private static ConfigurationManager getInstance(){
+		if(instance == null){
+			instance = new ConfigurationManager();
+			instance.prop = new Properties();
+		}
+		return instance;
+	}
+	
+	public synchronized static void loadPropertiesFile(String propFile){
+		InputStream in;
+		ConfigurationManager cm = getInstance();
+		try {
+			 in = cm.getClass().getClassLoader().getResourceAsStream(propFile);
+			
+			//in = new FileInputStream(url);
+			cm.prop.load(in);; //TODO check ecrasement
+			in.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static String getProperty(String key){
+		return getInstance().prop.getProperty(key);
+	}
+	
+}
